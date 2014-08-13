ce3d870cdeaffb97d410321fdf961db6ed8c5955
alec <azadikian1@hwemail.com>
2014-08-12 17:42:34 -0700

    Initial Commit

---
+++ libctalk/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  libctalk
+//
+//  Created by Alec Zadikian on 8/12/14.
+//  Copyright (c) 2014 AlecZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
