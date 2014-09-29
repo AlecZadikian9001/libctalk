38eae47d4cbd77447c4590bccb5e51660422fc8e
Alec Zadikian <AlecZ@1337.local>
2014-09-29 11:22:39 -0700

    Added sending and receiving functions that take custom functions for sending/receiving (untested).

--- libctalk/ctalk.h
+++ libctalk/ctalk.h
@@ -15,12 +15,20 @@ These functions send and receive individual "messages" (arrays of bytes of any v
 //======
 
 // Sends the data in message over file descriptor fd with a header indicating the size, messageLen. Returns 0 (error) if the system cannot send the data.
+
+// cTalkSendF takes a function parameter f for sending data using a custom function.
+// f must take a header, its length, the message, and the message length and send the header immediately preceding the message (similar to writev). It must return the number of bytes sent, or -1 upon a sending error.
 //
 size_t cTalkSend(int fd, unsigned char* message, size_t messageLen);
+size_t cTalkSendF(size_t (*f)(unsigned char* f_header, size_t f_headerLen, unsigned char* f_message, size_t f_messageLen), unsigned char* message, size_t messageLen);
 
 // Receives data on file descriptor fd without header and puts it in buffer. For the purpose of security and/or stability, a maximum buffer size bufferLen must be specified. Returns 0 (error) if the received data header indicates a size greater than bufferLen or if data with an invalid header is received. In either case, the system read buffer for fd is NOT flushed.
+
+// cTalkRecvF takes a function parameter for receiving data using a custom function.
+// f must take a buffer and its length, bufferLen, and read bufferLen bytes into the buffer (similar to recv or read). It must return the number of bytes read, or a number less than or equal to zero upon a reading error.
 //
 size_t cTalkRecv(int fd, unsigned char* buffer, size_t bufferLen);
+size_t cTalkRecvF(size_t (*f)(unsigned char* f_buffer, size_t f_bufferLen), unsigned char* buffer, size_t bufferLen);
 
 //======
 
